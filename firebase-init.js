aabeb28adef6ae3cdadf29f8adbbb4a8be2390fb
strbsecret911 <kiosinternett@gmail.com>
2025-12-26 20:05:46 +0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBN2PxHOA9u-I2iPCX_5gT1iogL5zYGHhM",
   authDomain: "antrianky.firebaseapp.com",
@@ -13,9 +9,9 @@ const firebaseConfig = {
   storageBucket: "antrianky.firebasestorage.app",
   messagingSenderId: "194560917480",
   appId: "1:194560917480:web:1aa1faa95f7e30c747ae5d",
-  measurementId: "G-2WVLM97MP4"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
